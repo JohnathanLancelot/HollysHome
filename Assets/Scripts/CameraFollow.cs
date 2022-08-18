72aa33c634cdb01ac00a7ef978f8d40373e8c3c7
Johnathan Lancelot <johnathan.lancelot@gmail.com>
2022-08-19 11:50:26 +1200

    Further attempts at getting the mouse controls working, no progress.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Vector3 offset;
+    public Vector3 offset = new Vector3(-1, 0, 0);
     public Transform target;
     //public float SmoothSpeed = 1.0f;
 
     void Update()
     {
         Vector3 DesiredPosition = target.position + offset;
+
         //Vector3 SmoothedPosition = Vector3.Lerp(transform.position, DesiredPosition, SmoothSpeed * Time.deltaTime);
         //transform.position = SmoothedPosition;
+
         transform.LookAt(target);
     }
 }
--- Assets/Scripts/Mouse.cs
+++ Assets/Scripts/Mouse.cs
@@ -7,6 +7,15 @@ public class Mouse : MonoBehaviour
     private Animator mouse;
     private bool Speed1 = true;
     private bool Speed2 = false;
+
+    CharacterController mouseController;
+    public float moveSpeed = 0.002f;
+    public Vector3 direction;
+    bool isIdle = true;
+
+    [SerializeField]
+    Transform mouseTransform;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,86 +40,117 @@ public class Mouse : MonoBehaviour
             mouse.SetBool("idle", true);
             mouse.SetBool("walk", false);
             mouse.SetBool("run", false);
+            isIdle = true;
         }
         if (Input.GetKeyUp(KeyCode.S))
         {
             mouse.SetBool("idle", true);
             mouse.SetBool("backward", false);
+            isIdle = true;
         }
         if ((Input.GetKeyDown(KeyCode.W))&&(Speed1==true))
         {
             mouse.SetBool("walk", true);
             mouse.SetBool("idle", false);
+            direction = new Vector3(moveSpeed, 0, 0);
+            isIdle = false;
         }
         if ((Input.GetKeyDown(KeyCode.W)) && (Speed2 == true))
         {
             mouse.SetBool("run", true);
             mouse.SetBool("idle", false);
+            direction = new Vector3((moveSpeed * 2), 0, 0);
+            isIdle = false;
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
             mouse.SetBool("backward", true);
             mouse.SetBool("idle", false);
+            direction = new Vector3((0 - moveSpeed), 0, 0);
+            isIdle = false;
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
             mouse.SetBool("turnleft", true);
             mouse.SetBool("idle", false);
+            direction = new Vector3(0, 0, (moveSpeed));
+            isIdle = false;
         }
         if (Input.GetKeyUp(KeyCode.A))
         {
             mouse.SetBool("idle", true);
             mouse.SetBool("turnleft", false);
+            isIdle = true;
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
             mouse.SetBool("turnright", true);
             mouse.SetBool("idle", false);
+            direction = new Vector3(0, 0, (0 - moveSpeed));
+            isIdle = false;
+
         }
         if (Input.GetKeyUp(KeyCode.D))
         {
             mouse.SetBool("idle", true);
             mouse.SetBool("turnright", false);
+            isIdle = true;
         }
         if ((Input.GetKeyDown(KeyCode.A))&&(Speed2==true))
         {
             mouse.SetBool("runleft", true);
             mouse.SetBool("run", false);
+            direction = new Vector3(0, 0, (moveSpeed * 2));
+            isIdle = false;
         }
         if ((Input.GetKeyUp(KeyCode.A))&&(Speed2==true))
         {
             mouse.SetBool("run", true);
             mouse.SetBool("runleft", false);
+            direction = new Vector3(0, 0, (moveSpeed * 2));
+            isIdle = false;
         }
         if ((Input.GetKeyDown(KeyCode.D))&&(Speed2==true))
         {
             mouse.SetBool("runright", true);
             mouse.SetBool("run", false);
+            direction = new Vector3(0, 0, ((0 - moveSpeed) * 2));
+            isIdle = false;
         }
         if ((Input.GetKeyUp(KeyCode.D))&&(Speed2==true))
         {
             mouse.SetBool("run", true);
             mouse.SetBool("runright", false);
+            direction = new Vector3(0, 0, ((0 - moveSpeed) * 2));
+            isIdle = false;
         }
         if ((Input.GetKeyDown(KeyCode.A)) && (Speed1 == true))
         {
             mouse.SetBool("walkleft", true);
             mouse.SetBool("walk", false);
+            direction = new Vector3(0, 0, moveSpeed);
+            isIdle = false;
         }
         if ((Input.GetKeyUp(KeyCode.A)) && (Speed1 == true))
         {
             mouse.SetBool("walk", true);
             mouse.SetBool("walkleft", false);
+            direction = new Vector3(0, 0, moveSpeed);
+            isIdle = false;
         }
         if ((Input.GetKeyDown(KeyCode.D)) && (Speed1 == true))
         {
             mouse.SetBool("walkright", true);
             mouse.SetBool("walk", false);
+            direction = new Vector3(0, 0, (0 - moveSpeed));
+            isIdle = false;
         }
         if ((Input.GetKeyUp(KeyCode.D)) && (Speed1 == true))
         {
             mouse.SetBool("walk", true);
             mouse.SetBool("walkright", false);
+            direction = new Vector3(0, 0, (0 - moveSpeed));
+            isIdle = false;
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -118,11 +158,13 @@ public class Mouse : MonoBehaviour
             mouse.SetBool("idle", false);
             mouse.SetBool("walk", false);
             mouse.SetBool("run", false);
+            isIdle = true;
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
             mouse.SetBool("idle", true);
             mouse.SetBool("jump", false);
+            isIdle = true;
         }
         if (Input.GetKey(KeyCode.E))
         {
@@ -130,11 +172,13 @@ public class Mouse : MonoBehaviour
             mouse.SetBool("idle", false);
             mouse.SetBool("walk", false);
             mouse.SetBool("run", false);
+            isIdle = true;
         }
         if (Input.GetKeyUp(KeyCode.E))
         {
             mouse.SetBool("idle", true);
             mouse.SetBool("eat", false);
+            isIdle = true;
         }
         if (Input.GetKey(KeyCode.F))
         {
@@ -142,11 +186,13 @@ public class Mouse : MonoBehaviour
             mouse.SetBool("idle", false);
             mouse.SetBool("walk", false);
             mouse.SetBool("run", false);
+            isIdle = true;
         }
         if (Input.GetKeyUp(KeyCode.F))
         {
             mouse.SetBool("attack", false);
             mouse.SetBool("idle", true);
+            isIdle = true;
         }
         if (Input.GetKey(KeyCode.K))
         {
@@ -154,6 +200,17 @@ public class Mouse : MonoBehaviour
             mouse.SetBool("idle", false);
             mouse.SetBool("walk", false);
             mouse.SetBool("run", false);
+            isIdle = true;
+        }
+        if (isIdle == true)
+        {
+            direction = new Vector3(0, 0, 0);
+        }
+
+        // Get the position of the mouse and add the vector to it:
+        if (isIdle == false)
+        {
+            mouseTransform.position += direction;
         }
     }
 }
