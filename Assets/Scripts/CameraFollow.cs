a9f71b6bf1fe4323d3cb16922631cf821553a30d
Johnathan Lancelot <johnathan.lancelot@gmail.com>
2022-08-19 18:58:48 +1200

    Mouse can now be controlled using the arrow or w,s,a,d keys

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Vector3 offset = new Vector3(-0.35f, 0.16f, 0);
+    public Vector3 offset = new Vector3(-0.41f, 0.03f, 0);
     public Transform target;
 
     void Update()
--- Assets/Scripts/Mouse.cs
+++ Assets/Scripts/Mouse.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Mouse : MonoBehaviour
 {
     private Animator mouse;
-    bool Speed1 = true;
-    bool Speed2 = false;
 
     CharacterController mouseController;
-    public float moveSpeed = 0.00002f;
+
+    public float moveSpeed = 1.3f;
+    public float rotationSpeed = 720;
+
     public Vector3 direction;
 
     [SerializeField]
@@ -28,140 +29,177 @@ public class Mouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveZ = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed * -1;
-        moveX = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        //moveZ = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed * -1;
+        //moveX = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        moveZ = Input.GetAxis("Horizontal") * -1;
+        moveX = Input.GetAxis("Vertical");
 
         Vector3 moveDir = new Vector3(moveX, 0, moveZ);
-        mouseController.Move(moveDir);
 
-        if (Input.GetKeyUp(KeyCode.W))
+        // Might make things less jittery:
+        moveDir.Normalize();
+
+        // TOO FAST:
+        //mouseController.Move(moveDir);
+
+        // JUST RIGHT SPEED:
+        transform.Translate(moveDir * moveSpeed * Time.deltaTime, Space.World);
+
+        if (moveDir != Vector3.zero)
         {
-            mouse.SetBool("idle", true);
-            mouse.SetBool("walk", false);
-            mouse.SetBool("run", false);
-        }
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            mouse.SetBool("idle", true);
-            mouse.SetBool("backward", false);
-        }
-        if ((Input.GetKeyDown(KeyCode.W)) && (Speed1 == true))
-        {
-            mouse.SetBool("walk", true);
+            transform.forward = moveDir;
+
             mouse.SetBool("idle", false);
-        }
-        if ((Input.GetKeyDown(KeyCode.W)) && (Speed2 == true))
-        {
             mouse.SetBool("run", true);
-            mouse.SetBool("idle", false);
         }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            mouse.SetBool("backward", true);
-            mouse.SetBool("idle", false);
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            mouse.SetBool("turnleft", true);
-            mouse.SetBool("idle", false);
-        }
-        if (Input.GetKeyUp(KeyCode.A))
+        else
         {
             mouse.SetBool("idle", true);
-            mouse.SetBool("turnleft", false);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            mouse.SetBool("turnright", true);
-            mouse.SetBool("idle", false);
-        }
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            mouse.SetBool("idle", true);
-            mouse.SetBool("turnright", false);
-        }
-        if ((Input.GetKeyDown(KeyCode.A)) && (Speed2 == true))
-        {
-            mouse.SetBool("runleft", true);
             mouse.SetBool("run", false);
         }
-        if ((Input.GetKeyUp(KeyCode.A)) && (Speed2 == true))
-        {
-            mouse.SetBool("run", true);
-            mouse.SetBool("runleft", false);
-        }
-        if ((Input.GetKeyDown(KeyCode.D)) && (Speed2 == true))
-        {
-            mouse.SetBool("runright", true);
-            mouse.SetBool("run", false);
-        }
-        if ((Input.GetKeyUp(KeyCode.D)) && (Speed2 == true))
-        {
-            mouse.SetBool("run", true);
-            mouse.SetBool("runright", false);
-        }
-        if ((Input.GetKeyDown(KeyCode.A)) && (Speed1 == true))
-        {
-            mouse.SetBool("walkleft", true);
-            mouse.SetBool("walk", false);
-        }
-        if ((Input.GetKeyUp(KeyCode.A)) && (Speed1 == true))
-        {
-            mouse.SetBool("walk", true);
-            mouse.SetBool("walkleft", false);
-        }
-        if ((Input.GetKeyDown(KeyCode.D)) && (Speed1 == true))
-        {
-            mouse.SetBool("walkright", true);
-            mouse.SetBool("walk", false);
-        }
-        if ((Input.GetKeyUp(KeyCode.D)) && (Speed1 == true))
-        {
-            mouse.SetBool("walk", true);
-            mouse.SetBool("walkright", false);
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            mouse.SetBool("jump", true);
-            mouse.SetBool("idle", false);
-            mouse.SetBool("walk", false);
-            mouse.SetBool("run", false);
-        }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            mouse.SetBool("idle", true);
-            mouse.SetBool("jump", false);
-        }
-        if (Input.GetKey(KeyCode.E))
-        {
-            mouse.SetBool("eat", true);
-            mouse.SetBool("idle", false);
-            mouse.SetBool("walk", false);
-            mouse.SetBool("run", false);
-        }
-        if (Input.GetKeyUp(KeyCode.E))
-        {
-            mouse.SetBool("idle", true);
-            mouse.SetBool("eat", false);
-        }
-        if (Input.GetKey(KeyCode.F))
-        {
-            mouse.SetBool("attack", true);
-            mouse.SetBool("idle", false);
-            mouse.SetBool("walk", false);
-            mouse.SetBool("run", false);
-        }
-        if (Input.GetKeyUp(KeyCode.F))
-        {
-            mouse.SetBool("attack", false);
-            mouse.SetBool("idle", true);
-        }
-        if (Input.GetKey(KeyCode.K))
-        {
-            mouse.SetBool("death", true);
-            mouse.SetBool("idle", false);
-            mouse.SetBool("walk", false);
-            mouse.SetBool("run", false);
-        }
+
+    //    if ((Input.GetKeyUp(KeyCode.UpArrow)) || (Input.GetKeyUp(KeyCode.W)))
+    //    {
+    //        mouse.SetBool("idle", true);
+    //        mouse.SetBool("walk", false);
+    //    }
+    //    if ((Input.GetKeyUp(KeyCode.DownArrow)) || (Input.GetKeyUp(KeyCode.S)))
+    //    {
+    //        mouse.SetBool("idle", true);
+    //        mouse.SetBool("backward", false);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.UpArrow))
+    //    {
+    //        mouse.SetBool("walk", true);
+    //        mouse.SetBool("idle", false);
+    //        mouse.SetBool("walkleft", false);
+    //        mouse.SetBool("walkright", false);
+    //        mouse.SetBool("backward", false);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.W))
+    //    {
+    //        mouse.SetBool("walk", true);
+    //        mouse.SetBool("idle", false);
+    //        mouse.SetBool("walkleft", false);
+    //        mouse.SetBool("walkright", false);
+    //        mouse.SetBool("backward", false);
+    //    }
+    //    if ((Input.GetKeyDown(KeyCode.DownArrow)) || (Input.GetKeyDown(KeyCode.S)))
+    //    {
+    //        mouse.SetBool("backward", true);
+    //        mouse.SetBool("idle", false);
+    //        mouse.SetBool("walk", false);
+    //        mouse.SetBool("walkleft", false);
+    //        mouse.SetBool("walkright", false);
+    //    }
+    //    if ((Input.GetKeyDown(KeyCode.LeftArrow)) || (Input.GetKeyDown(KeyCode.A)))
+    //    {
+    //        mouse.SetBool("walkleft", true);
+    //        mouse.SetBool("idle", false);
+    //        mouse.SetBool("walk", false);
+    //        mouse.SetBool("walkright", false);
+    //    }
+    //    if ((Input.GetKeyUp(KeyCode.LeftArrow)) || (Input.GetKeyUp(KeyCode.A)))
+    //    {
+    //        mouse.SetBool("idle", true);
+    //        mouse.SetBool("walkleft", false);
+    //    }
+    //    if ((Input.GetKeyDown(KeyCode.RightArrow)) || (Input.GetKeyDown(KeyCode.D)))
+    //    {
+    //        mouse.SetBool("walkright", true);
+    //        mouse.SetBool("idle", false);
+    //        mouse.SetBool("backward", false);
+    //        mouse.SetBool("walk", false);
+    //        mouse.SetBool("walkleft", false);
+    //    }
+    //    if ((Input.GetKeyUp(KeyCode.RightArrow)) || (Input.GetKeyUp(KeyCode.D)))
+    //    {
+    //        mouse.SetBool("idle", true);
+    //        mouse.SetBool("walkright", false);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.LeftArrow))
+    //    {
+    //        mouse.SetBool("walkleft", true);
+    //        mouse.SetBool("walk", false);
+    //        mouse.SetBool("backward", false);
+    //        mouse.SetBool("idle", false);
+    //        mouse.SetBool("walkright", false);
+    //    }
+    //    if (Input.GetKeyUp(KeyCode.LeftArrow))
+    //    {
+    //        mouse.SetBool("walk", true);
+    //        mouse.SetBool("walkleft", false);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.RightArrow))
+    //    {
+    //        mouse.SetBool("walkright", true);
+    //        mouse.SetBool("walk", false);
+    //        mouse.SetBool("backward", false);
+    //        mouse.SetBool("idle", false);
+    //        mouse.SetBool("walkleft", false);
+    //    }
+    //    if (Input.GetKeyUp(KeyCode.RightArrow))
+    //    {
+    //        mouse.SetBool("walk", true);
+    //        mouse.SetBool("walkright", false);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.A))
+    //    {
+    //        mouse.SetBool("walkleft", true);
+    //        mouse.SetBool("walk", false);
+    //        mouse.SetBool("backward", false);
+    //        mouse.SetBool("idle", false);
+    //        mouse.SetBool("walkright", false);
+    //    }
+    //    if (Input.GetKeyUp(KeyCode.A))
+    //    {
+    //        mouse.SetBool("walk", true);
+    //        mouse.SetBool("walkleft", false);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.D))
+    //    {
+    //        mouse.SetBool("walkright", true);
+    //        mouse.SetBool("walk", false);
+    //        mouse.SetBool("backward", false);
+    //        mouse.SetBool("idle", false);
+    //        mouse.SetBool("walkleft", false);
+    //    }
+    //    if (Input.GetKeyUp(KeyCode.D))
+    //    {
+    //        mouse.SetBool("walk", true);
+    //        mouse.SetBool("walkright", false);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        mouse.SetBool("jump", true);
+    //        mouse.SetBool("idle", false);
+    //        mouse.SetBool("walk", false);
+    //        mouse.SetBool("run", false);
+    //    }
+    //    if (Input.GetKeyUp(KeyCode.Space))
+    //    {
+    //        mouse.SetBool("idle", true);
+    //        mouse.SetBool("jump", false);
+    //    }
+    //    if (Input.GetKey(KeyCode.E))
+    //    {
+    //        mouse.SetBool("eat", true);
+    //        mouse.SetBool("idle", false);
+    //        mouse.SetBool("walk", false);
+    //        mouse.SetBool("run", false);
+    //    }
+    //    if (Input.GetKeyUp(KeyCode.E))
+    //    {
+    //        mouse.SetBool("idle", true);
+    //        mouse.SetBool("eat", false);
+    //    }
+    //    if (Input.GetKey(KeyCode.K))
+    //    {
+    //        mouse.SetBool("death", true);
+    //        mouse.SetBool("idle", false);
+    //        mouse.SetBool("walk", false);
+    //        mouse.SetBool("run", false);
+    //    }
     }
 }
